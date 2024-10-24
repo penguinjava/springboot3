ff3ab2328ec994ae3776b7eca88de915de2c1763
penguinjava <aiueoeom97@gmail.com>
2024-10-24 23:29:24 +0900

    수정

---
+++ src/main/java/org/example/springbootdeveloper/TestController.java
@@ -0,0 +1,32 @@
+package org.example.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//프레젠테이션 계층
+@RestController
+public class TestController{
+    @Autowired // 빈을 주입
+    TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers(){ // 제네릭 메서드
+        List<Member> members = testService.getAllMembers();
+        return members;
+    }
+}
+
+//비즈니스 계층
+@Service
+public class TestService{
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll();
+    }
+}
